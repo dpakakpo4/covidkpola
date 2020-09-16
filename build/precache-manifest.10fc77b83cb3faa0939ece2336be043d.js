871b9d3f216bd70ca2ff5c6434679225cb60ced2
Akakpo Kodjovi Denis-Papin <dpakakpo@gmail.com>
2020-09-16 23:17:46 +0000

    firibase init and deploy

---
+++ build/precache-manifest.10fc77b83cb3faa0939ece2336be043d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb465d64f9645736b38ca17b93ceb2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bbb79142ffb52072aab",
+    "url": "/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "a852e501e5006758a4fa",
+    "url": "/static/css/main.e4bc8822.chunk.css"
+  },
+  {
+    "revision": "8bbb79142ffb52072aab",
+    "url": "/static/js/2.ed5b11cd.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.ed5b11cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a852e501e5006758a4fa",
+    "url": "/static/js/main.345d8f21.chunk.js"
+  },
+  {
+    "revision": "7c5dd75b0ccc52984ba8",
+    "url": "/static/js/runtime-main.6f7677a4.js"
+  }
+]);
